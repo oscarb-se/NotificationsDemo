2db9ea26b6435ccd286c00dbbf190dae45c0ece7
Oscar <github@oscarb.se>
2016-03-15 14:45:29 +0100

    Added method showing a notification

--- app/src/main/java/se/oscarb/notificationsdemo/MainActivity.java
+++ app/src/main/java/se/oscarb/notificationsdemo/MainActivity.java
@@ -1,7 +1,10 @@
 package se.oscarb.notificationsdemo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.NotificationManager;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    // Metod för att visa en notifiering (notification)
+    public void showNotification(View view)  {
+
+        // Vi behöver bygga ihop en Notification först
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+
+        // Bestäm hur den ska se ut (följande tre saker krävs)
+        builder.setSmallIcon(R.mipmap.ic_launcher);
+        builder.setContentTitle("Titel på notification");
+        builder.setContentText("Text på notification!");
+
+        int notificationId = 1; // Ett påhittat ID för att hänvisa till samma senare
+
+        // Hämta systemet som visar notifications
+        NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        manager.notify(notificationId, builder.build());
     }
 }
